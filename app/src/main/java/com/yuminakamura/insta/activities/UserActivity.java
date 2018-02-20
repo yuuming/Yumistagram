befb76f4c6607630d68990c4d48b5f6d2e24d820
Yuumi <yuuming.n@gmail.com>
2018-02-20 01:40:35 -0800

    Create navigationbar

--- app/src/main/java/com/yuminakamura/insta/activities/UserActivity.java
+++ app/src/main/java/com/yuminakamura/insta/activities/UserActivity.java
@@ -1,43 +1,44 @@
 package com.yuminakamura.insta.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-import com.yuminakamura.insta.R;
-import com.yuminakamura.insta.fragments.UserImageDetailFragment;
-import com.yuminakamura.insta.fragments.UserImageListFragment;
-import com.yuminakamura.insta.models.DataSource;
-import com.yuminakamura.insta.models.UserPost;
+public class UserActivity extends AppCompatActivity {
 
+    private TextView mTextMessage;
 
-import java.util.ArrayList;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-public class UserActivity extends AppCompatActivity implements UserImageListFragment.userImageSelected {
-
-    public ArrayList<UserPost> userPostsLists = DataSource.getUserPostLists();
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(R.id.fragment_container, UserImageListFragment.newInstance(), "userImageList")
-                    .commit();
-        }
-
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    @Override
-    public void userImageSelected() {
-
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.fragment_container, UserImageDetailFragment.newInstance(), "imageDetail")
-                .addToBackStack(null)
-                .commit();
-    }
 }
